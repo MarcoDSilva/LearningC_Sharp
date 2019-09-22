0dc6e73fc9aba4e320806e14569b388f98ffa177
MarcoDSilva <38049196+MarcoDSilva@users.noreply.github.com>
2019-09-22 18:30:40 +0000

    Update StringConnection.cs

--- CSharp_DB_ActiviesAndUser/proj_prod_msilva/StringConnection.cs
+++ CSharp_DB_ActiviesAndUser/proj_prod_msilva/StringConnection.cs
@@ -8,6 +8,6 @@ namespace proj_prod_msilva
 {
     class StringConnection
     {
-        public static string SC = "Data Source=89.154.2.41,62444;Initial Catalog = ommited; User ID = ommited; Password=123.abc;Connect Timeout = 30; Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        public static string SC = "Data Source=*omitted*;Initial Catalog = ommited; User ID = ommited; Password=*omitted*Connect Timeout = 30; Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
     }
 }
